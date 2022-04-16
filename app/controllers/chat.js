d5321dc5957d1c51965f8ac10bb112d0c93ff512
Felipe Tavares <felipeat07@gmail.com>
2022-04-16 13:28:51 +0100

    validando dados do form, req via post

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,16 @@
 module.exports.iniciaChat = function(application, req, res){
+
+    var dadosForm = req.body;
+
+    req.assert('apelido','Apelido ou nome é obrigatório!').notEmpty();
+    req.assert('apelido','Nome ou Apelido deve conter entre 3 e 15 caracteres').len(3,15);
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render('index', {validacao : errors})
+        return;
+    }
+
     res.render('chat');
 }
