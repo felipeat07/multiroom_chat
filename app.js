a7979c26455a2a1f6ca45d9afee54c6e22a28568
Felipe Tavares <felipeat07@gmail.com>
2022-04-18 12:23:52 +0100

    enviando menssagem que o usuário se conectou ao chat

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var server = app.listen(80, function(){
 
 var io = require('socket.io').listen(server);
 
+//Deixando a variável global para poder ser usada no chat.js
+app.set('io', io);
+
 //  Criar a conexão por websocket
 io.on('connection', function(socket){
     console.log('Usuário conectou')
